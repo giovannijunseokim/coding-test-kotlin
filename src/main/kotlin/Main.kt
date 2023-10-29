6e2fd979ac09970c1b0a1d764b933ea21087723f
giovannijunseokim <kingjunseo@gmail.com>
2023-10-29 19:56:39 +0900

    프로그래머스 "과일장수" 문제 개선

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,25 +5,15 @@ fun main() {
 class Solution {
     fun solution(k: Int, m: Int, score: IntArray): Int {
         var profitSum = 0
+        score.sortDescending()
 
         var fruitIndex = 0
-        val sortedScore = score.sortedDescending()
         while (fruitIndex <= score.size - m) {
-            val fruitBox = mutableSetOf<Int>()
-            for (index in fruitIndex until fruitIndex + m)
-                fruitBox.add(sortedScore[index])
-            profitSum += fruitBox.getMin() * m
+            val fruitBox = score.slice(fruitIndex until fruitIndex + m)
+            profitSum += fruitBox.last() * m
             fruitIndex += m
         }
 
         return profitSum
     }
-}
-
-private fun Set<Int>.getMin(): Int {
-    var min = first()
-    forEach {
-        if (it < min) min = it
-    }
-    return min
 }
