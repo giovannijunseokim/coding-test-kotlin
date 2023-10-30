043f071e46d51aa4417ddcdb869e0a90643573c7
giovannijunseokim <kingjunseo@gmail.com>
2023-10-30 18:16:51 +0900

    프로그래머스 "기사단원의 무기" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,28 @@
-import java.util.*
+import kotlin.math.sqrt
 
 fun main() {
-
+    println(Solution().solution(10, 3, 2))
 }
 
 class Solution {
-    fun solution(k: Int, scores: IntArray): IntArray {
-        val answerList: MutableList<Int> = mutableListOf()
-        val scorePriorityQueue: PriorityQueue<Int> = PriorityQueue()
-        scores.forEach { score ->
-            scorePriorityQueue.add(score)
-            if (scorePriorityQueue.size > k) scorePriorityQueue.poll()
-            answerList.add(scorePriorityQueue.peek())
-        }
+    fun solution(number: Int, limit: Int, limitPower: Int): Int {
 
-        return answerList.toIntArray()
+        var answer: Int = 0
+        // 모든 기사 반복
+        for (knight in 1..number) {
+            val factors = calculateFactors(knight)
+            answer += if (factors > limit) limitPower else factors
+        }
+        return answer
+    }
+
+    private fun calculateFactors(number: Int): Int {
+        var factorCount = 0
+        val rootNumberToInt = sqrt(number.toDouble()).toInt()
+        for (index in 1..rootNumberToInt) {
+            if (index * index == number) factorCount++
+            else if (number % index == 0) factorCount += 2
+        }
+        return factorCount
     }
 }
