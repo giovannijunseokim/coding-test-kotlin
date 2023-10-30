202b6b219b033260c280f1eb52a28d128b74b9e4
giovannijunseokim <kingjunseo@gmail.com>
2023-10-30 18:17:14 +0900

    프로그래머스 "기사단원의 무기" 문제 개선

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,23 @@
-import kotlin.math.sqrt
-
 fun main() {
     println(Solution().solution(10, 3, 2))
 }
 
 class Solution {
-    fun solution(number: Int, limit: Int, limitPower: Int): Int {
-
-        var answer: Int = 0
-        // 모든 기사 반복
-        for (knight in 1..number) {
-            val factors = calculateFactors(knight)
-            answer += if (factors > limit) limitPower else factors
-        }
-        return answer
+    fun solution(number: Int, limit: Int, power: Int): Int {
+        return IntArray(number) { index -> calculateFactors(index + 1) }
+            .fold(0) { ironAmount, knightPower ->
+                if (knightPower > limit) ironAmount + power
+                else ironAmount + knightPower
+            }
     }
 
     private fun calculateFactors(number: Int): Int {
         var factorCount = 0
-        val rootNumberToInt = sqrt(number.toDouble()).toInt()
-        for (index in 1..rootNumberToInt) {
-            if (index * index == number) factorCount++
-            else if (number % index == 0) factorCount += 2
+        var index = 1
+        while (index * index < number) {
+            if (number % index++ == 0) factorCount += 2
         }
+        if (index * index == number) factorCount++
         return factorCount
     }
 }
