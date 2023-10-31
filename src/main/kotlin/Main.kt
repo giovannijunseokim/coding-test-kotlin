7fc60e90f1632ab858403e3f977affabf16e76ce
giovannijunseokim <kingjunseo@gmail.com>
2023-10-31 14:22:57 +0900

    프로그래머스 "옹알이" 문제 개선

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,30 +4,12 @@ fun main() {
 
 class Solution {
     fun solution(babbling: Array<String>): Int {
-        var answer = 0
-        val wordsCanSpeak = arrayListOf("aya", "ye", "woo", "ma")
+        val speakableRegex = "aya|ye|woo|ma".toRegex()
+        val unspeakableRegex = "ayaaya|yeye|woowoo|mama".toRegex()
 
-        babbling.forEach { word ->
-            var removableWord = word
-
-            for (index in 0 until wordsCanSpeak.size) {
-                val wordCanSpeak = wordsCanSpeak[index]
-                var tempStartIndex: Int? = null
-
-                while (removableWord.contains(wordCanSpeak)) {
-                    val startIndex = removableWord.indexOf(wordCanSpeak)
-                    if (tempStartIndex == startIndex) break
-                    removableWord = removableWord.replaceRange(startIndex until startIndex + wordCanSpeak.length, " ")
-                    tempStartIndex = startIndex + 1
-                }
-
-                if (removableWord.isBlank()) {
-                    answer++
-                    break
-                }
-            }
-        }
-
-        return answer
+        val speakableWords: List<String> = babbling.map { it.replace(regex = unspeakableRegex, "*") }
+            .map { it.replace(regex = speakableRegex, "") }
+            .filter { it == "" }
+        return speakableWords.size
     }
 }
