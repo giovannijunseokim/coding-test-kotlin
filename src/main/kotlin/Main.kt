a8b523b59fa69d0d8314cf537eccd9d89d4c66d1
giovannijunseokim <kingjunseo@gmail.com>
2023-11-02 17:56:15 +0900

    프로그래머스 "문자열 나누기" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,50 @@
 fun main() {
-    println(Solution().solution(arrayOf("ayayeaya", "ayaaya", "woowooma", "mawooyeaya")))
+    println(Solution().solution("abracadabra"))
 }
 
 class Solution {
-    fun solution(babbling: Array<String>): Int {
-        val speakableRegex = "aya|ye|woo|ma".toRegex()
-        val unspeakableRegex = "ayaaya|yeye|woowoo|mama".toRegex()
+    fun solution(s: String): Int {
+        var answer = 0
 
-        val speakableWords: List<String> = babbling.map { it.replace(regex = unspeakableRegex, "*") }
-            .map { it.replace(regex = speakableRegex, "") }
-            .filter { it == "" }
-        return speakableWords.size
+        var sameCharCount = 0
+        var diffCharCount = 0
+        var firstChar: Char? = null
+        var charIndex = 0
+
+        while (charIndex < s.length) {
+            if (firstChar == null) {
+                firstChar = s[charIndex]
+                sameCharCount++
+                charIndex++
+            }
+
+            if (charIndex == s.length) {
+                answer++
+                break
+            }
+
+            if (firstChar == s[charIndex]) {
+                sameCharCount++
+            } else {
+                diffCharCount++
+            }
+            charIndex++
+
+            if (charIndex == s.length) {
+                answer++
+                break
+            }
+
+            if (sameCharCount == diffCharCount) {
+                firstChar = null
+                sameCharCount = 0
+                diffCharCount = 0
+
+                answer++
+                continue
+            }
+        }
+
+        return answer
     }
 }
