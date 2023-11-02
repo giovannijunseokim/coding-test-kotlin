36911a407adf1edd19de7a3977199514aa6887ef
giovannijunseokim <kingjunseo@gmail.com>
2023-11-02 17:58:08 +0900

    프로그래머스 "문자열 나누기" 문제 개선

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,45 +6,17 @@ class Solution {
     fun solution(s: String): Int {
         var answer = 0
 
-        var sameCharCount = 0
-        var diffCharCount = 0
-        var firstChar: Char? = null
-        var charIndex = 0
-
-        while (charIndex < s.length) {
-            if (firstChar == null) {
-                firstChar = s[charIndex]
-                sameCharCount++
-                charIndex++
-            }
-
-            if (charIndex == s.length) {
+        val stack = mutableListOf<Char>()
+        s.forEach { char ->
+            if (stack.isEmpty()) {
+                stack.add(char)
                 answer++
-                break
-            }
-
-            if (firstChar == s[charIndex]) {
-                sameCharCount++
+            } else if (stack.first() == char) {
+                stack.add(char)
             } else {
-                diffCharCount++
-            }
-            charIndex++
-
-            if (charIndex == s.length) {
-                answer++
-                break
-            }
-
-            if (sameCharCount == diffCharCount) {
-                firstChar = null
-                sameCharCount = 0
-                diffCharCount = 0
-
-                answer++
-                continue
+                stack.removeFirst()
             }
         }
-
         return answer
     }
 }
