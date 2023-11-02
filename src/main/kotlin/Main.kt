3fdd8fcca6994f1636a6070624fb940fd0470d2f
giovannijunseokim <kingjunseo@gmail.com>
2023-11-02 22:53:12 +0900

    프로그래머스 "대충 만든 자판" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,38 @@
+import java.lang.Integer.min
+
 fun main() {
-    println(Solution().solution("abracadabra"))
+    println(Solution().solution(arrayOf("ACGQ", "BAC"), arrayOf("ABCTGBBC", "AAGQBABBABBZ")))
 }
 
 class Solution {
-    fun solution(s: String): Int {
-        var answer = 0
-
-        val stack = mutableListOf<Char>()
-        s.forEach { char ->
-            if (stack.isEmpty()) {
-                stack.add(char)
-                answer++
-            } else if (stack.first() == char) {
-                stack.add(char)
-            } else {
-                stack.removeFirst()
+    fun solution(keymap: Array<String>, targets: Array<String>): IntArray {
+        val answer = mutableListOf<Int>()
+        targets.forEach { target ->
+            var btnClickCount = 0
+            for (targetCharIndex in target.indices) { // target[targetCharIndex] == 한 타겟의 chars
+                var minBtnCount: Int? = null
+                for (btnIndex in keymap.indices) { // keymap[btnIndex]는 각각의 버튼들
+                    if (!keymap[btnIndex].contains(target[targetCharIndex])) {
+                        continue
+                    }
+                    var tempClickCount = 0 // target의 한 char에 대한 클릭 횟수
+                    while (tempClickCount < keymap[btnIndex].length) {
+                        if (target[targetCharIndex] == keymap[btnIndex][tempClickCount]) {
+                            minBtnCount = if (minBtnCount == null) tempClickCount + 1
+                            else min(minBtnCount, tempClickCount + 1)
+                            break
+                        }
+                        tempClickCount++
+                    }
+                }
+                if (minBtnCount == null) {
+                    // 버튼들을 통해 char를 만들 수 없음 -> -1을 넣어야 함
+                    btnClickCount = -1
+                    break
+                } else btnClickCount += minBtnCount // char마다 버튼 클릭 횟수의 총합을 넣어야 하므로
             }
+            answer.add(btnClickCount)
         }
-        return answer
+        return answer.toIntArray()
     }
 }
