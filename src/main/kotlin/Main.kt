92af17a192911f80e883688e62d7c761c9a30e7e
giovannijunseokim <kingjunseo@gmail.com>
2023-11-07 16:08:01 +0900

    프로그래머스 "바탕화면 정리" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,27 @@
 fun main() {
-    println(Solution().solution(arrayOf("OSO", "OOO", "OXO", "OOO"), arrayOf("E 2", "S 3", "W 1")))
+    Solution().also { println(it.solution(arrayOf(".#...", "..#..", "...#."))) }
 }
 
 class Solution {
-    /** 출발 위치 찾기 */
-    private fun findStart(park: Array<String>): MutableList<Int> {
-        for (y in park.indices)
-            for (x in park[y].indices)
-                if (park[y][x] == 'S')
-                    return mutableListOf(y, x)
-        return mutableListOf(0, 0)
-    }
+    fun solution(wallpaper: Array<String>): IntArray {
+        val answer: IntArray
 
-    fun solution(park: Array<String>, routes: Array<String>): IntArray {
-        /** 방향 입력 시 방향으로 한 칸 변환하는 Pair 를 얻는 Map */
-        val directions = mapOf('E' to (0 to 1), 'W' to (0 to -1), 'N' to (-1 to 0), 'S' to (1 to 0))
-        /** route 배열을 방향, step Pair 의 배열로 변경 */
-        return routes.map { it[0] to it.drop(2).toInt() }
-            .fold(findStart(park)) { currentPosition, (direction, distance) ->
-                val prevPosition = currentPosition.toMutableList() // [Y축 위치, X축 위치]
-                val nextPosition = currentPosition.toMutableList() // [Y축 위치, X축 위치]
-                repeat(distance) {
-                    nextPosition[0] += directions[direction]!!.first // Y축 위치 변경 (1칸씩)
-                    nextPosition[1] += directions[direction]!!.second // X축 위치 변경 (1칸씩)
-                    if (!(0 <= nextPosition[0] && nextPosition[0] < park.size && 0 <= nextPosition[1] && nextPosition[1] < park[0].length && park[nextPosition[0]][nextPosition[1]] != 'X'))
-                        return@fold prevPosition
+        var startPosition = Pair(wallpaper.size - 1, wallpaper[0].length)
+        var endPosition = Pair(0, 0)
+
+        for (y in wallpaper.indices)
+            for (x in wallpaper.first().indices) {
+                println("$y, $x")
+                if (wallpaper[y][x] == '#') {
+                    println("matched : $y, $x")
+                    if (y < startPosition.first) startPosition = Pair(y, startPosition.second)
+                    if (x < startPosition.second) startPosition = Pair(startPosition.first, x)
+                    if (y + 1 > endPosition.first) endPosition = Pair(y + 1, endPosition.second)
+                    if (x + 1 > endPosition.second) endPosition = Pair(endPosition.first, x + 1)
                 }
-                return@fold nextPosition
-            }.toIntArray()
+            }
+        println("$startPosition, $endPosition")
+        answer = (startPosition.toList() + endPosition.toList()).toIntArray()
+        return answer
     }
 }
