a9214c40521a933d9a49b73c24cb799eb3a26333
giovannijunseokim <kingjunseo@gmail.com>
2023-11-07 11:48:39 +0900

    프로그래머스 "공원 산책" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,92 @@
 fun main() {
-    println(Solution().solution(arrayOf("ACGQ", "BAC"), arrayOf("ABCTGBBC", "AAGQBABBABBZ")))
+    println(Solution().solution(arrayOf("OSO", "OOO", "OXO", "OOO"), arrayOf("E 2", "S 3", "W 1")))
 }
 
 class Solution {
-    fun solution(keymap: Array<String>, targets: Array<String>): IntArray {
-        var answer = listOf<Int>()
-        // 리스트를 바꿈
-        answer = targets.map { target ->
-            // 각 target에 대한 리스트 -> 최소 키 리스트
-            target.map { char ->
-                // target의 char들에 대해
-                keymap.map { btn -> btn.indexOf(char) + 1 }.filterNot { btnClickCount ->
-                    btnClickCount < 1
-                }.let { btnClickCountList ->
-                    btnClickCountList.minOrNull() ?: -1
-                } // char에 대한 버튼별 클릭 횟수 리스트
-            }.let { clicksCharNeed ->
-                if (clicksCharNeed.contains(-1)) -1
-                else clicksCharNeed.sum()
+    fun solution(park: Array<String>, routes: Array<String>): IntArray {
+        val minX = 0
+        val minY = 0
+
+        val maxX = park.first().length - 1
+        val maxY = park.size - 1
+
+        var currentX: Int = -1
+        var currentY: Int = -1
+
+        park.forEachIndexed { startStringIndex, string ->
+            val startCharIndex = string.indexOf('S')
+            if (startCharIndex != -1) {
+                currentX = startCharIndex
+                currentY = startStringIndex
+                return@forEachIndexed
             }
         }
 
-        return answer.toIntArray()
+        fun checkCanMove(opCode: Char, step: Int): Boolean {
+            var tempX = currentX
+            var tempY = currentY
+
+            when (opCode) {
+                'E' -> {
+                    if ((tempX + step) !in minX..maxX) return false
+                    for (index in 0 until step) {
+                        tempX++
+                        if (park[tempY][tempX] == 'X') return false
+                    }
+                }
+
+                'W' -> {
+                    if ((tempX - step) !in minX..maxX) return false
+                    for (index in 0 until step) {
+                        tempX--
+                        if (park[tempY][tempX] == 'X') return false
+                    }
+                }
+
+                'S' -> {
+                    if ((tempY + step) !in minY..maxY) return false
+                    for (index in 0 until step) {
+                        tempY++
+                        if (park[tempY][tempX] == 'X') return false
+                    }
+                }
+
+                'N' -> {
+                    if ((tempY - step) !in minY..maxY) return false
+                    for (index in 0 until step) {
+                        tempY--
+                        if (park[tempY][tempX] == 'X') return false
+                    }
+                }
+            }
+            return true
+        }
+
+        fun moveTo(opCode: Char, step: Int) {
+            if (!checkCanMove(opCode, step)) return
+            when (opCode) {
+                'E' -> {
+                    currentX += step
+                }
+
+                'W' -> {
+                    currentX -= step
+                }
+
+                'S' -> {
+                    currentY += step
+                }
+
+                'N' -> {
+                    currentY -= step
+                }
+            }
+        }
+
+        routes.forEach { route ->
+            moveTo(route.first(), Character.getNumericValue(route.last()))
+            println("$currentY, $currentX")
+        }
+        return intArrayOf(currentY, currentX)
     }
 }
