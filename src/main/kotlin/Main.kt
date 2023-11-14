ed16d77931543f4378cdb075784da93416d10314
giovannijunseokim <kingjunseo@gmail.com>
2023-11-14 19:18:31 +0900

    프로그래머스 "나머지가 1이 되는 수 찾기" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,23 +2,17 @@ fun main() {
     Solution().also {
         println(
             it.solution(
-                ingredient = intArrayOf(1,2,1,2,3,1,3,1,2,4)
+                5
             )
         )
     }
 }
 
 class Solution {
-    fun solution(ingredient: IntArray): Int {
-        var answer: Int = 0
-        val sb = StringBuilder()
-        for (item in ingredient) {
-            sb.append('0' + item)
-            if (sb.length >= 4 && sb.substring(sb.length - 4) == "1231") {
-                sb.setLength(sb.length - 4)
-                answer++
-            }
+    fun solution(n: Int): Int {
+        for (index in 2 until n) {
+            if (n % index == 1) return index
         }
-        return answer
+        throw Exception()
     }
 }
