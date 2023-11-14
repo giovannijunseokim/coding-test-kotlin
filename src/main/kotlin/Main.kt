26247efc7a47c4c92633146426593b6ef574d606
giovannijunseokim <kingjunseo@gmail.com>
2023-11-14 19:20:17 +0900

    프로그래머스 "나머지가 1이 되는 수 찾기" 문제 개선

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,11 @@
 fun main() {
     Solution().also {
-        println(
-            it.solution(
-                5
-            )
-        )
+        println(it.solution(5))
     }
 }
 
 class Solution {
     fun solution(n: Int): Int {
-        for (index in 2 until n) {
-            if (n % index == 1) return index
-        }
-        throw Exception()
+        return (2..n).first { n % it == 1 }
     }
 }
