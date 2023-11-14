847e1ea846e8eccc91e20dd6690dff0c6582aca3
giovannijunseokim <kingjunseo@gmail.com>
2023-11-14 19:09:23 +0900

    프로그래머스 "햄버거 만들기" 문제 개선

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ fun main() {
     Solution().also {
         println(
             it.solution(
-                ingredient = intArrayOf(2, 1, 1, 2, 3, 1, 2, 3, 1)
+                ingredient = intArrayOf(1,2,1,2,3,1,3,1,2,4)
             )
         )
     }
@@ -11,26 +11,14 @@ fun main() {
 class Solution {
     fun solution(ingredient: IntArray): Int {
         var answer: Int = 0
-        var index = 0
-        val mutableIngredient = ingredient.toMutableList()
-        while (index + 3 < mutableIngredient.size) {
-            if (mutableIngredient[index] == 빵 && mutableIngredient[index + 1] == 야채 &&
-                mutableIngredient[index + 2] == 고기 && mutableIngredient[index + 3] == 빵
-            ) {
-                mutableIngredient.removeAt(index + 3)
-                mutableIngredient.removeAt(index + 2)
-                mutableIngredient.removeAt(index + 1)
-                mutableIngredient.removeAt(index)
-                index = if (index < 3) 0 else index - 3
+        val sb = StringBuilder()
+        for (item in ingredient) {
+            sb.append('0' + item)
+            if (sb.length >= 4 && sb.substring(sb.length - 4) == "1231") {
+                sb.setLength(sb.length - 4)
                 answer++
-            } else index++
+            }
         }
         return answer
-    }
-
-    companion object {
-        const val 빵 = 1
-        const val 야채 = 2
-        const val 고기 = 3
     }
 }
