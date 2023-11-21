267315418d99526eca3a26b8cc346356a4c0eb13
giovannijunseokim <kingjunseo@gmail.com>
2023-11-21 18:06:00 +0900

    프로그래머스 "실패율" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,13 @@
 fun main() {
-    println(Solution().solution(3, intArrayOf(1), intArrayOf(1)))
+    println(Solution().solution(5, intArrayOf(1, 1, 1, 2, 3, 4)).toList())
 }
 
 class Solution {
-    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
-        val lostStudents = (lost.sorted().toSet() - reserve.sorted().toSet()).toMutableSet()
-        val reserveStudents = (reserve.sorted().toSet() - lost.sorted().toSet())
+    fun solution(N: Int, stages: IntArray): IntArray = (1..N).map { stage ->
+        stages.filter { it == stage }.size.toDouble()
+            .div(stages.filter { it >= stage }.size.toDouble().takeUnless { it == 0.0 } ?: 1.0)
+    }.withIndex().sortedByDescending {
+        it.value
+    }.map { it.index + 1 }.toIntArray()
 
-        reserveStudents.forEach { reserveStudent ->
-            if (lostStudents.contains(reserveStudent - 1)) {
-                lostStudents.remove(reserveStudent - 1)
-            } else if (lostStudents.contains(reserveStudent + 1)) {
-                lostStudents.remove(reserveStudent + 1)
-            }
-        }
-        return n - lostStudents.size
-    }
 }
