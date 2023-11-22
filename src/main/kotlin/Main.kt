a664a1cb36976ef3f79eacf297fbe8c3e000bb9b
giovannijunseokim <kingjunseo@gmail.com>
2023-11-22 13:45:54 +0900

    프로그래머스 "크레인 인형 뽑기" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,34 @@
+import java.util.*
+
 fun main() {
-    println(Solution().solution(5, intArrayOf(1, 1, 1, 2, 3, 4)).toList())
+    println(
+        Solution().solution(
+            arrayOf(
+                intArrayOf(0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 1, 0, 3),
+                intArrayOf(0, 2, 5, 0, 1),
+                intArrayOf(4, 2, 4, 4, 2),
+                intArrayOf(3, 5, 1, 3, 1)
+            ), intArrayOf(1, 5, 3, 5, 1, 2, 1, 4)
+        )
+    )
 }
 
 class Solution {
-    fun solution(N: Int, stages: IntArray): IntArray = (1..N).map { stage ->
-        stages.filter { it == stage }.size.toDouble()
-            .div(stages.filter { it >= stage }.size.toDouble().takeUnless { it == 0.0 } ?: 1.0)
-    }.withIndex().sortedByDescending {
-        it.value
-    }.map { it.index + 1 }.toIntArray()
-
+    fun solution(board: Array<IntArray>, moves: IntArray): Int {
+        var answer = 0
+        val basket = Stack<Int>()
+        moves.forEach { move ->
+            for (y in board.indices) {
+                if (board[y][move - 1] == 0) continue
+                if (basket.takeIf { it.isNotEmpty() }?.peek() == board[y][move - 1]) {
+                    basket.pop()
+                    answer += 2
+                } else basket.push(board[y][move - 1])
+                board[y][move - 1] = 0
+                break
+            }
+        }
+        return answer
+    }
 }
