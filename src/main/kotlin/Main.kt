3674538239f8eada89d6842cf11b086d0b50c5f0
giovannijunseokim <kingjunseo@gmail.com>
2023-11-22 16:06:31 +0900

    프로그래머스 "키패드 누르기" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,61 @@
-import java.util.*
+import kotlin.math.absoluteValue
 
 fun main() {
-    println(
-        Solution().solution(
-            arrayOf(
-                intArrayOf(0, 0, 0, 0, 0),
-                intArrayOf(0, 0, 1, 0, 3),
-                intArrayOf(0, 2, 5, 0, 1),
-                intArrayOf(4, 2, 4, 4, 2),
-                intArrayOf(3, 5, 1, 3, 1)
-            ), intArrayOf(1, 5, 3, 5, 1, 2, 1, 4)
-        )
-    )
+    println(Solution().solution(intArrayOf(1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5), "right"))
 }
 
 class Solution {
-    fun solution(board: Array<IntArray>, moves: IntArray): Int {
-        var answer = 0
-        val basket = Stack<Int>()
-        moves.forEach { move ->
-            for (y in board.indices) {
-                if (board[y][move - 1] == 0) continue
-                if (basket.takeIf { it.isNotEmpty() }?.peek() == board[y][move - 1]) {
-                    basket.pop()
-                    answer += 2
-                } else basket.push(board[y][move - 1])
-                board[y][move - 1] = 0
-                break
+    fun solution(numbers: IntArray, hand: String): String {
+        val sb = StringBuilder()
+        val keypadMap = mapOf(
+            '1' to (0 to 0),
+            '2' to (0 to 1),
+            '3' to (0 to 2),
+            '4' to (1 to 0),
+            '5' to (1 to 1),
+            '6' to (1 to 2),
+            '7' to (2 to 0),
+            '8' to (2 to 1),
+            '9' to (2 to 2),
+            '0' to (3 to 1)
+        )
+        var leftHandPosition = 3 to 0
+        var rightHandPosition = 3 to 2
+        for (number in numbers) {
+            val numberPosition: Pair<Int, Int> = keypadMap['0' + number]!!
+            if (number in 1..7 step 3) {
+                sb.append('L')
+                leftHandPosition = keypadMap['0' + number]!!
+                continue
+            }
+            if (number in 3..9 step 3) {
+                sb.append('R')
+                rightHandPosition = numberPosition
+                continue
+            }
+
+            val leftHandDistance =
+                (numberPosition.first - leftHandPosition.first).absoluteValue + (numberPosition.second - leftHandPosition.second).absoluteValue
+            val rightHandDistance =
+                (numberPosition.first - rightHandPosition.first).absoluteValue + (numberPosition.second - rightHandPosition.second).absoluteValue
+
+            if (leftHandDistance < rightHandDistance) {
+                sb.append('L')
+                leftHandPosition = numberPosition
+            } else if (leftHandDistance > rightHandDistance) {
+                sb.append('R')
+                rightHandPosition = numberPosition
+            } else {
+                if (hand == "left") {
+                    sb.append('L')
+                    leftHandPosition = numberPosition
+                }
+                if (hand == "right") {
+                    sb.append('R')
+                    rightHandPosition = numberPosition
+                }
             }
         }
-        return answer
+        return sb.toString()
     }
 }
