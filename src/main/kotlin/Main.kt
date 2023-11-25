30edf2f07db636eb69f187aa825ac57f2f5c94bf
giovannijunseokim <kingjunseo@gmail.com>
2023-11-25 16:19:53 +0900

    프로그래머스 "3진법 뒤집기" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,52 +1,19 @@
-import kotlin.math.absoluteValue
+import kotlin.math.pow
 
 fun main() {
-    println(Solution().solution(intArrayOf(1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5), "right"))
+    println(Solution().solution(45))
 }
 
 class Solution {
-    fun solution(numbers: IntArray, hand: String): String {
-        val sb = StringBuilder()
-        var leftHandPosition = 3 to 0
-        var rightHandPosition = 3 to 2
-        for (number in numbers) {
-            val numberPosition: Pair<Int, Int> = keypadArray[number]
-            if (number in 1..7 step 3) {
-                sb.append('L')
-                leftHandPosition = numberPosition
-                continue
-            }
-            if (number in 3..9 step 3) {
-                sb.append('R')
-                rightHandPosition = numberPosition
-                continue
-            }
-
-            val leftHandDistance =
-                (numberPosition.first - leftHandPosition.first).absoluteValue + (numberPosition.second - leftHandPosition.second).absoluteValue
-            val rightHandDistance =
-                (numberPosition.first - rightHandPosition.first).absoluteValue + (numberPosition.second - rightHandPosition.second).absoluteValue
-
-            if (leftHandDistance < rightHandDistance) {
-                sb.append('L')
-                leftHandPosition = numberPosition
-            } else if (leftHandDistance > rightHandDistance) {
-                sb.append('R')
-                rightHandPosition = numberPosition
-            } else {
-                if (hand == "left") {
-                    sb.append('L')
-                    leftHandPosition = numberPosition
-                }
-                if (hand == "right") {
-                    sb.append('R')
-                    rightHandPosition = numberPosition
-                }
-            }
+    fun solution(n: Int): Int {
+        var answer: Int = n
+        val ternary: MutableList<Int> = mutableListOf()
+        while (answer >= 3) {
+            ternary.add(answer % 3)
+            answer /= 3
         }
-        return sb.toString()
+        return ternary.plus(answer).reversed().mapIndexed { index, element ->
+            element * 3.toDouble().pow(index)
+        }.sum().toInt()
     }
 }
-
-val keypadArray =
-    arrayOf(3 to 1, 0 to 0, 0 to 1, 0 to 2, 1 to 0, 1 to 1, 1 to 2, 2 to 0, 2 to 1, 2 to 2)
