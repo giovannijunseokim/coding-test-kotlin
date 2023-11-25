43b37d7399d5b40ca24a63963c254c34d5139b82
giovannijunseokim <kingjunseo@gmail.com>
2023-11-25 16:20:31 +0900

    프로그래머스 "3진법 뒤집기" 문제 개선

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,7 @@
-import kotlin.math.pow
-
 fun main() {
     println(Solution().solution(45))
 }
 
 class Solution {
-    fun solution(n: Int): Int {
-        var answer: Int = n
-        val ternary: MutableList<Int> = mutableListOf()
-        while (answer >= 3) {
-            ternary.add(answer % 3)
-            answer /= 3
-        }
-        return ternary.plus(answer).reversed().mapIndexed { index, element ->
-            element * 3.toDouble().pow(index)
-        }.sum().toInt()
-    }
+    fun solution(n: Int): Int = n.toString(3).reversed().toInt(3)
 }
