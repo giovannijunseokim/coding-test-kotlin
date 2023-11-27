e2bdbbec7a4804f16529b342cbb2bd168185533d
giovannijunseokim <kingjunseo@gmail.com>
2023-11-27 16:51:29 +0900

    프로그래머스 "신규 아이디 추천" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,21 @@
 fun main() {
-    println(Solution().solution(intArrayOf(1, 2, 3, 4), intArrayOf(-3, -1, 0, 2)))
+    println(Solution().solution("...!@BaT#*..y.abcdefghijklm"))
 }
 
 class Solution {
-    fun solution(a: IntArray, b: IntArray): Int {
-        return a.zip(b).sumOf { it.first * it.second }
+    fun solution(new_id: String): String {
+        return new_id.lowercase().filter { it in 'a'..'z' || it in '0'..'9' || it == '-' || it == '_' || it == '.' }
+            .let {
+                it.filterIndexed { index, c ->
+                    if (index < it.length - 1) {
+                        c != '.' || it[index + 1] != '.'
+                    } else true
+                }
+            }.replace("..", ".").removePrefix(".").removeSuffix(".").let {
+                if (it.isEmpty()) "a"
+                else if (it.length >= 16) {
+                    it.substring(0 until 15).removeSuffix(".")
+                } else it
+            }.let { if (it.length == 1) it.repeat(3) else if (it.length == 2) it.plus(it.last()) else it }
     }
 }
