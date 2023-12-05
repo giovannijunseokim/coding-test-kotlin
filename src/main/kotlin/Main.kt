600007ad47204ea448df8b894dbfd73b4edc23fe
giovannijunseokim <kingjunseo@gmail.com>
2023-12-05 16:41:36 +0900

    프로그래머스 "신고 결과 받기" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,32 @@
 fun main() {
     println(
         Solution().solution(
-            arrayOf("AN", "CF", "MJ", "RT", "NA"), intArrayOf(5, 3, 2, 7, 5)
+            arrayOf("muzi", "frodo", "apeach", "neo"),
+            arrayOf("muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"),
+            2
         )
     )
 }
 
 class Solution {
-    fun solution(survey: Array<String>, choices: IntArray): String {
-        val scoreMap = mutableMapOf('R' to 0, 'T' to 0, 'C' to 0, 'F' to 0, 'J' to 0, 'M' to 0, 'A' to 0, 'N' to 0)
-        choices.zip(survey).forEach {
-            if (it.first > 4) {
-                scoreMap[it.second.last()] = scoreMap[it.second.last()]!!.plus(it.first - 4)
-            }
-            if (it.first < 4) {
-                scoreMap[it.second.first()] = scoreMap[it.second.first()]!!.plus(4 - it.first)
+    fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
+
+        val idReportMap: MutableMap<String, MutableSet<String>> = id_list.associateWith {
+            mutableSetOf<String>()
+        }.toMutableMap().apply {
+            report.forEach { this[it.substringBefore(' ')]?.add(it.substringAfter(' ')) }
+        }
+
+        val idReportedMap: MutableMap<String, Int> = mutableMapOf<String, Int>().apply {
+            idReportMap.forEach {
+                it.value.forEach { reportedUser ->
+                    if (this[reportedUser] != null) this[reportedUser] = this[reportedUser]!! + 1
+                    else this[reportedUser] = 1
+                }
             }
         }
-        StringBuilder().apply {
-            append(if (scoreMap['R']!! >= scoreMap['T']!!) 'R' else 'T')
-            append(if (scoreMap['C']!! >= scoreMap['F']!!) 'C' else 'F')
-            append(if (scoreMap['J']!! >= scoreMap['M']!!) 'J' else 'M')
-            append(if (scoreMap['A']!! >= scoreMap['N']!!) 'A' else 'N')
-        }.also { return it.toString() }
+
+        return idReportMap.map { it.value.count { idReportedMap.filter { it.value >= k }.map { it.key }.contains(it) } }
+            .toIntArray()
     }
 }
