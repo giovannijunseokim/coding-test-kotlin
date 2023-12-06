747f9f0edc7cad84529084fe85037a09855ce2e5
giovannijunseokim <kingjunseo@gmail.com>
2023-12-06 15:15:31 +0900

    프로그래머스 "숫자 짝꿍" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,31 @@
 fun main() {
     println(
         Solution().solution(
-            arrayOf("muzi", "frodo", "apeach", "neo"),
-            arrayOf("muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"),
-            2
+            "12321", "42531"
         )
     )
 }
 
 class Solution {
-    fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
-        println(report.map { it.split(" ") }
-            .groupBy { it[1] }.asSequence().map { it.value.distinct() })
-        return report.map { it.split(" ") }
-            .groupBy { it[1] }
-            .asSequence()
-            .map { it.value.distinct() }
-            .filter { it.size >= k }
-            .flatten()
-            .map { it[0] }
-            .groupingBy { it }
-            .eachCount()
-            .run { id_list.map { getOrDefault(it, 0) }.toIntArray() }
-    }
+    fun solution(X: String, Y: String): String {
+        val sb = StringBuilder()
+        var xIndex = 0
+        var yIndex = 0
 
+        val x = X.toList().sortedDescending()
+        val y = Y.toList().sortedDescending()
+
+        while (xIndex < X.length && yIndex < Y.length) {
+            if (x[xIndex] == y[yIndex]) {
+                sb.append(x[xIndex])
+                xIndex++
+                yIndex++
+            } else if (x[xIndex] > y[yIndex]) xIndex++
+            else yIndex++
+        }
+        val answer = sb.toString()
+        if (answer.isBlank()) return "-1"
+        if (answer.first() == '0') return "0"
+        return answer
+    }
 }
