204a69fbfd4751091ba01ca3459eb56323a18b90
giovannijunseokim <kingjunseo@gmail.com>
2023-12-07 15:41:58 +0900

    프로그래머스 "소수 만들기" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,21 @@
 fun main() {
     println(
-        Solution().solution(
-            intArrayOf(1, 5, 2, 6, 3, 7, 4), arrayOf(intArrayOf(2, 5, 3), intArrayOf(4, 4, 1), intArrayOf(1, 7, 3))
-        )
+        Solution().solution(intArrayOf(1, 2, 3, 4))
     )
 }
 
 class Solution {
-    fun solution(array: IntArray, commands: Array<IntArray>): IntArray =
-        commands.map { array.toList().subList(it[0] - 1, it[1]).sorted()[it[2] - 1] }.toIntArray()
+    fun solution(nums: IntArray): Int {
+        var answer = 0
+
+        for (first in 0 until nums.size - 2) for (second in first + 1 until nums.size - 1) for (third in second + 1 until nums.size) {
+            (nums[first] + nums[second] + nums[third]).let { num ->
+                if ((1..num).filter { num % it == 0 }.size == 2) {
+                    answer++
+                }
+            }
+        }
+
+        return answer
+    }
 }
