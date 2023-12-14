a9960698dd33878e819ef9705e8cd07153e882b9
giovannijunseokim <kingjunseo@gmail.com>
2023-12-14 19:22:51 +0900

    프로그래머스 "문자열 내 마음대로 정렬하기" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,15 @@
-import java.time.LocalDate
-
 fun main() {
     println(
-        Solution().solution(5, 24)
+        Solution().solution(arrayOf("abce", "abcd", "cdx"), 2)
     )
 }
 
 class Solution {
-    fun solution(a: Int, b: Int): String = LocalDate.of(2016, a, b).dayOfWeek.toString().substring(0..2)
+    fun solution(strings: Array<String>, n: Int) = strings.sortedWith { s1, s2 ->
+        if (s1[n] == s2[n]) {
+            if (s1 > s2) 1 else -1
+        } else if (s1[n] > s2[n]) {
+            1
+        } else -1
+    }
 }
