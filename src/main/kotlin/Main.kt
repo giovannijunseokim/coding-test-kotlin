94fcb1f9510c27793a960255df838081ae1dfba9
giovannijunseokim <kingjunseo@gmail.com>
2023-12-21 16:54:03 +0900

    프로그래머스 "이진 변환 반복하기" 문제 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,19 @@
 fun main() {
     println(
-        Solution().solution("for the last week")
+        Solution().solution("110010101001")
     )
 }
 
 class Solution {
-    fun solution(s: String): String = s.split(' ').joinToString(separator = " ") {
-        it.mapIndexed { index, c -> if (index == 0) c.uppercase() else c.lowercase() }.joinToString(separator = "")
+    fun solution(s: String): IntArray {
+        var string = s
+        var count1 = 0
+        var round = 0
+        while (string != "1") {
+            string.forEach { if (it == '0') count1++ }
+            string = string.filterNot { it == '0' }.length.toString(2)
+            round++
+        }
+        return intArrayOf(round, count1)
     }
 }
